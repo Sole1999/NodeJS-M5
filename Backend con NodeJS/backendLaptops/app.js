1ce22dc03510a0831b8be58f32306b26a7ed8a4e
Sole1999 <141879281+Sole1999@users.noreply.github.com>
2025-11-01 22:46:41 -0500

    Reto 2 Consultar Laptops con Expo.

---
+++ Backend con NodeJS/backendLaptops/app.js
@@ -0,0 +1,69 @@
+const exp = require("express");
+
+const app = exp();
+const bodyParser = require("body-parser");
+const puerto = 2000
+
+const cors = require("cors");
+
+app.use(cors()); // ← ACTIVA CORS
+
+
+app.use(bodyParser.json());
+
+app.use("/laptops", (solicitud, respuesta, next)=>{
+    console.log("HEADERS: ", solicitud.headers);
+    console.log("BODY: ", solicitud.body);
+    next();
+});
+
+app.post("/laptops", (req , resp)=>{
+    id = req.body.id = 100;
+    resp.send(req.body);
+    console.log("id:", id);
+});
+
+app.get("/laptops/:id",(req, resp)=>{
+
+    const lpts = [
+         {id: 1, marca: "HP", procesador: "RYZEN 7", memoria: "62 GB", disco: "2 TB"},
+         {id: 2, marca: "MAC", procesador: "CORE i9", memoria: "102 GB", disco: "1 TB"},
+         {id: 3, marca: "LENOVO", procesador: "RYZEN 7", memoria: "32 GB", disco: "1 TB"},
+         {id: 4, marca: "DELL", procesador: "CORE i6", memoria: "62 GB", disco: "328 GB"}
+     ];
+
+    const ide = req.params.id;
+    //resp.send( req.marca, req.procesador, req.memoria, req.body.disco);
+    console.log("id:", ide);
+    resp.send(req.body);
+});
+
+app.get("/laptops",(req, resp)=>{
+
+    const lpts = [
+         {id: 1, marca: "HP", procesador: "RYZEN 7", memoria: "62 GB", disco: "2 TB"},
+         {id: 2, marca: "MAC", procesador: "CORE i9", memoria: "102 GB", disco: "1 TB"},
+         {id: 3, marca: "LENOVO", procesador: "RYZEN 7", memoria: "32 GB", disco: "1 TB"},
+         {id: 4, marca: "DELL", procesador: "CORE i6", memoria: "62 GB", disco: "328 GB"}
+     ];
+    //resp.send(req.body);
+    resp.send(lpts);
+});
+
+app.put("/laptops/:id",(req, resp)=>{
+
+    const ide = req.params.id;
+    console.log("id:", ide);
+    resp.send(req.body);
+});
+
+app.delete("/laptops/:id",(req, resp)=>{
+
+    const ide = req.params.id;
+    console.log("id:", ide);
+    resp.send();
+});
+
+app.listen(puerto,()=>{
+    console.log("Servidor activo con el puerto:", puerto);
+});
---
+++ Backend con NodeJS/miPrimerProyecto/app.js
@@ -0,0 +1,55 @@
+const express = require("express"); //extrayendo el modulo con require en este caso a Express
+
+const app = express(); //llamamos y guardamos en una variable los metodos o funciones de Express
+
+const bodyParser = require("body-parser");
+
+const puerto = 3001;
+
+const cors = require("cors");
+
+app.use(cors()); // ← ACTIVA CORS
+
+app.use(bodyParser.json());
+
+app.use("/contactos", (req, resp, next)=>{
+    console.log("Ingresa a Middleware");
+    console.log("headers:", req.headers);
+    console.log("body:", req.body);
+    next();
+});
+
+app.get("/contactos",(request, response)=>{
+    const contactos = [
+        {id:1, nombre: "Santiago", apellido: "Mosquera", celular: "098765412"},
+        {id:2, nombre: "Sandra", apellido: "Romero", celular: "0985213654"},
+        {id:3, nombre: "Kevin", apellido: "Oro", celular: "0923415621"}
+    ];
+    console.log("Ingresa a GET");
+    response.send(contactos); 
+});
+
+app.post("/contactos",(pedido, repuesta)=>{
+    pedido.body.id =99;
+    repuesta.send(pedido.body);
+    //repuesta.send("post contactos");
+});
+
+app.put("/contactos/:idParam",(req, resp)=>{
+    const id = req.params.idParam;
+    console.log("id", id);
+    resp.send(req.body);
+    //resp.send("put contactos");
+});
+
+app.delete("/contactos/:id",(req, resp)=>{
+    const id = req.params.id;
+    console.log("id:", id);
+    resp.send();
+});
+
+//linea de codigo para levantar un servidor en Express 
+// se levanta con la funcion listen y dentro de ella se ubica como primer parametro el puerto y el segundo la funcion creada alli que queremos wque ejecute 
+app.listen(puerto,()=>{
+    console.log("Servidor listo en el puerto "+ puerto);
+});
