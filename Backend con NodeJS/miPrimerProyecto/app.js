ddc5eb6f8a8f46d660b0638f1e1e7748cacd25b2
Sole1999 <141879281+Sole1999@users.noreply.github.com>
2025-11-03 12:14:42 -0500

    Reto 6 Evidencia Postman de Contactos.

--- Backend con NodeJS/miPrimerProyecto/app.js
+++ Backend con NodeJS/miPrimerProyecto/app.js
@@ -8,48 +8,117 @@ const puerto = 3001;
 
 const cors = require("cors");
 
+const { Client } = require("pg");
+
+
+const client = new Client({
+    user: "postgres",
+    host: "192.168.1.26",  //localhost
+    database: "nodeJS",
+    password: "Postgresql.",
+    port: 5432
+});
+
 app.use(cors()); // ← ACTIVA CORS
 
 app.use(bodyParser.json());
 
-app.use("/contactos", (req, resp, next)=>{
+
+app.use("/contactos", (req, resp, next) => {
     console.log("Ingresa a Middleware");
     console.log("headers:", req.headers);
     console.log("body:", req.body);
     next();
 });
 
-app.get("/contactos",(request, response)=>{
-    const contactos = [
-        {id:1, nombre: "Santiago", apellido: "Mosquera", celular: "098765412"},
-        {id:2, nombre: "Sandra", apellido: "Romero", celular: "0985213654"},
-        {id:3, nombre: "Kevin", apellido: "Oro", celular: "0923415621"}
-    ];
+
+// METODO GET
+app.get("/contactos", (request, response) => {
+    // const contactos = [
+    //     {id:1, nombre: "Santiago", apellido: "Mosquera", celular: "098765412"},
+    //     {id:2, nombre: "Sandra", apellido: "Romero", celular: "0985213654"},
+    //     {id:3, nombre: "Kevin", apellido: "Oro", celular: "0923415621"}
+    // ];
     console.log("Ingresa a GET");
-    response.send(contactos); 
+
+    client.connect();
+
+    client.query("select * from contactos").then(responseQuery => {
+        console.log(responseQuery.rows);
+        response.send(responseQuery.rows);
+        //client.end();
+    }).catch(err => {
+        console.log(err);
+        client.end();
+    })
+
+    //response.send(contactos); 
+
+
 });
 
-app.post("/contactos",(pedido, repuesta)=>{
-    pedido.body.id =99;
-    repuesta.send(pedido.body);
+
+// METODO POST  (INSERTAR)
+app.post("/contactos", (pedido, repuesta) => {
+    //pedido.body.id = 99;
     //repuesta.send("post contactos");
+
+    client.query(
+        'insert into contactos (nombre, apellido, celular) values ($1, $2, $3);', [pedido.body.nombre, pedido.body.apellido, pedido.body.celular]
+    ).then(responseQuery => {
+        console.log(responseQuery.rows);
+        repuesta.send(responseQuery.rows);
+        //client.end();
+    }).catch(err => {
+        console.log(err);
+        client.end();
+    })
+
+    //repuesta.send(pedido.body);
 });
 
-app.put("/contactos/:idParam",(req, resp)=>{
+
+//METODO PUT  (ACTUALIZAR)
+app.put("/contactos/:idParam", (req, resp) => {
     const id = req.params.idParam;
     console.log("id", id);
-    resp.send(req.body);
+
+    client.query(
+        'UPDATE contactos set nombre = $1, apellido = $2, celular = $3 where id = $4', [req.body.nombre, req.body.apellido, req.body.celular, req.params.idParam]
+    ).then(responseQuery => {
+        console.log(responseQuery.rows);
+        resp.send(responseQuery.rows);
+        //client.end();
+    }).catch(err => {
+        console.log(err);
+        client.end();
+    })
+    //resp.send(req.body);
     //resp.send("put contactos");
 });
 
-app.delete("/contactos/:id",(req, resp)=>{
-    const id = req.params.id;
-    console.log("id:", id);
-    resp.send({id : id});
+
+//METODO DELETE
+app.delete("/contactos/:id", (req, resp) => {
+    //const id = req.params.id;
+    //console.log("id:", id);
+
+     client.query(
+        'delete from contactos where id = $1', [req.params.id]
+    ).then(responseQuery => {
+        console.log(responseQuery.rows);
+        resp.send(responseQuery.rows);
+        //client.end();
+    }).catch(err => {
+        console.log(err);
+        client.end();
+    })
+
+    //resp.send({ id: id });
 });
 
 //linea de codigo para levantar un servidor en Express 
 // se levanta con la funcion listen y dentro de ella se ubica como primer parametro el puerto y el segundo la funcion creada alli que queremos wque ejecute 
-app.listen(puerto,()=>{
-    console.log("Servidor listo en el puerto "+ puerto);
+app.listen(puerto, () => {
+    console.log("Servidor listo en el puerto " + puerto);
 });
